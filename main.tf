40cf194c62dfa96f7170517045fe544f0bc809db
Rachel Sharp <rachel@hashicorp.com>
2020-06-22 17:02:45 -0400

    Initial demo code

---
+++ main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = var.region
+}
+
+
+resource "random_pet" "petname" {
+  length    = 3
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "${var.prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "demo" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.demo.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  description = "This is the cloud hosting region where Terraform deploys your webapp"
+}
+
+variable "prefix" {
+  description = "This is the environemnt where Terraform deploys your webapp"
+}
