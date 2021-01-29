eef9ebbfa6ad1722440850f82446b2fb7cdec618
Rachel Sharp <rachel@hashicorp.com>
2021-01-28 23:43:18 -0500

    014updates

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+  backend "remote" {
+    organization = "hashicorp-rachel"
+    workspaces {
+      name = "sentinel-example"
+    }
+  }
+}
+
+
 provider "aws" {
   region = var.region
 }
