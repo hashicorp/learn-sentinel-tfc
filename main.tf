3f2dcb664006ed0802cbca5a06b24dd66ed94fa7
Rachel Sharp <rachel@hashicorp.com>
2021-02-10 14:50:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.26.0"
+    }
+  }
+  required_version = "~> 0.14"
+  backend "remote" {
+    organization = "<YOUR_TERRAFORM_ORG>"
+    workspaces {
+      name = "sentinel-example"
+    }
+  }
+}
+
+
 provider "aws" {
   region = var.region
 }
