be2d9e02fec1fbb8c0aa52ee8e66901dfb0c25af
James Haughey <james.haughey@capgemini.com>
2021-08-02 13:55:49 +0100

    Develop changes

--- main.tf
+++ main.tf
@@ -26,3 +26,12 @@ resource "aws_instance" "ubuntu" {
     Name = var.instance_name
   }
 }
+
+resource "aws_instance" "second" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+
+  tags = {
+    Name = var.instance_name
+  }
+}
--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "suspicious-tesla"
 
     workspaces {
       name = "sentinel-example"
