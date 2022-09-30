3f29610720c3d1785e82f2cce1360affae1393a1
Javier Palma <91437129+imjavierpalma@users.noreply.github.com>
2022-09-30 15:55:04 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "imjavierpalma"
 
     workspaces {
       name = "sentinel-example"
