9dd336f838b59dc65ec1432eab3159d55cabfc39
Kateryna Firoozi <kateryna.firoozi@soprasteria.com>
2023-04-13 09:58:06 +0200

    tester pre-commit

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "ubuntu" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "region" {
   description = "AWS region"
-  default = "eu-west-1"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default = "t2.micro"
+  default     = "t2.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default = "Provisioned by Terraform"
+  default     = "Provisioned by Terraform"
 }
